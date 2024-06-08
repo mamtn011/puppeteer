c00c24fdf6da4b8ab896e30485cfd4a1ce180920
Mobin <mamtn011@gmail.com>
2024-06-08 13:01:10 +0600

    added env.example

---
+++ env.example.js
@@ -0,0 +1,10 @@
+// web share free proxy
+// p.webshare.io:10009:pnimtfom-10:o168cow765xp
+// p.webshare.io:10007:pnimtfom-8:o168cow765xp
+// p.webshare.io:10005:pnimtfom-6:o168cow765xp
+// p.webshare.io:10000:pnimtfom-1:o168cow765xp
+
+// USA_PROXY=http://pnimtfom-10:o168cow765xp@p.webshare.io:10009
+// SPAIN_PROXY=http://pnimtfom-8:o168cow765xp@p.webshare.io:10007
+// ITALY_PROXY=http://pnimtfom-6:o168cow765xp@p.webshare.io:10005
+// NED_PROXY=http://pnimtfom-1:o168cow765xp@p.webshare.io:10000
--- sephora.mjs
+++ sephora.mjs
@@ -3,13 +3,13 @@ import { setTimeout } from "timers/promises";
 import "dotenv/config";
 import proxyChain from "proxy-chain";
 (async () => {
-  const newProxy = await proxyChain.anonymizeProxy(process.env.NED_PROXY);
+  const newProxy = await proxyChain.anonymizeProxy(process.env.USA_PROXY);
 
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width: 1500, height: 1000 },
     userDataDir: "temporary",
-    // args: [`--proxy-server=${newProxy}`],
+    args: [`--proxy-server=${newProxy}`],
     ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
